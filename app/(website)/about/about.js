4a06e679b94479a2d5e655fbb0950caed57b2da2
Benjamin-Nyakambangwe <benjaminnyakambangwe@gmail.com>
2023-09-25 19:19:30 +0200

    im back

--- app/(website)/about/about.js
+++ app/(website)/about/about.js
@@ -6,20 +6,20 @@ import Link from "next/link";
 export default function About({ authors, settings }) {
   return (
     <Container>
-      <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
+      <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
         About
       </h1>
       <div className="text-center">
         <p className="text-lg">We are a small passionate team.</p>
       </div>
 
-      <div className="grid grid-cols-3 gap-5 mt-6 mb-16 md:mt-16 md:mb-32 md:gap-16">
+      <div className="mb-16 mt-6 grid grid-cols-3 gap-5 md:mb-32 md:mt-16 md:gap-16">
         {authors.slice(0, 3).map(author => {
           const imageProps = urlForImage(author?.image) || null;
           return (
             <div
               key={author._id}
-              className="relative overflow-hidden rounded-md aspect-square odd:translate-y-10 odd:md:translate-y-16">
+              className="relative aspect-square overflow-hidden rounded-md odd:translate-y-10 odd:md:translate-y-16">
               <Link href={`/author/${author.slug}`}>
                 <Image
                   src={imageProps.src}
@@ -34,17 +34,19 @@ export default function About({ authors, settings }) {
         })}
       </div>
 
-      <div className="mx-auto prose text-center dark:prose-invert mt-14">
+      <div className="prose mx-auto mt-14 text-center dark:prose-invert">
         <p>
-          We provide real-time connectivity to enable software
-          providers and financial institutions to build integrated
-          products for their small business customers.
+          Welcome to Tactical Zone, your ultimate destination for all
+          things tactical gaming. We're here to help you discover
+          tips, tricks, and guides for various tactical game modes,
+          along with the latest news and in-depth analysis from the
+          world of tactical gaming.
         </p>
         <p>
-          Our API infrastructure is leveraged by clients ranging from
-          lenders to corporate card providers and business forecasting
-          tools, with use cases including automatic reconciliation,
-          business dashboarding, and loan decisioning.
+          Whether you're a seasoned player or just beginning your
+          journey, Tactical Zone is your go-to source for sharpening
+          your skills and staying informed in the realm of tactical
+          gaming. Join us and elevate your gaming experience today.
         </p>
         <p>
           <Link href="/contact">Get in touch</Link>
